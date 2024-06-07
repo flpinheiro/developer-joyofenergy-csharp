9a44c11f2feb9ab84233a36042c4e645136f44c8
Felipe Luís Pinheiro <flpinheiro@gmail.com>
2024-06-07 17:14:54 -0300

    initial commit

---
+++ JOIEnergy.Tests/ElectricityReadingIntervalTest.cs
@@ -0,0 +1,19 @@
+﻿using JOIEnergy.Domain;
+using System;
+using Xunit;
+
+namespace JOIEnergy.Tests;
+
+public class ElectricityReadingIntervalTest
+{
+    public ElectricityReadingIntervalTest()
+    {
+    }
+
+    [Fact]
+    public void GetLastWeekFromToday()
+    {
+        Assert.Equal(new DateTime(2021, 9, 26), ElectricityReadingInterval.getPreviousWeekInterval(new DateTime(2021, 10, 5, 12, 00, 00)).Start);
+        Assert.Equal(new DateTime(2021, 10, 3), ElectricityReadingInterval.getPreviousWeekInterval(new DateTime(2021, 10, 5, 12, 00, 00)).End);
+    }
+}
